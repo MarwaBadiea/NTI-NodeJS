fdb81d7bcfa612de0e28c2297dced1abeabda7d9
MarwaBadiea <78789651+MarwaBadiea@users.noreply.github.com>
2021-06-09 02:24:17 +0200

    Add files via upload

---
+++ Task 1/main.js
@@ -0,0 +1,25 @@
+
+let todoList = document.querySelector('input');
+let btn = document.querySelector('button');
+let output = document.querySelector('ul');
+let liElement = document.createElement('li');
+let child = output.appendChild(liElement);
+
+btn.addEventListener('click', function () {
+    child.innerHTML = todoList.value;
+    todoList.value = '';
+    liElement = document.createElement('li');
+    child = output.appendChild(liElement);
+});
+
+
+(async function fetchTodoList() {
+    const response = await fetch('http://jsonplaceholder.typicode.com/todos')
+    const data = await response.json()
+    for (let i=0; i<10; i++) {
+        console.log(data[i]);
+        liElement = document.createElement('li');
+        child = output.appendChild(liElement);
+        child.innerHTML = 'UserId: ' + data[i].userId + ',' + ' id: ' + data[i].id + ',' + ' Title: ' + data[i].title + ',' + ' Completed: ' + data[i].completed;
+    }
+})();
